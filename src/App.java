e967f0aca1e475d8a6522f2ed40fe71fbf1ad326
Carlos <samtard.cp@gmail.com>
2022-12-11 05:38:51 -0500

    commit inicial

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+public Master
+  public estatic void main(string arg[]){
+  System.out.println("hola mundo")
+
+  }
